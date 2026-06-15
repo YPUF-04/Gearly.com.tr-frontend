ac8eb5a171a4f5b9581fb53ae72c576f2fd41fe0
byott01 <Yusufitayfur@gmail.com>
2026-06-16 02:10:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // =============================================
 // CONFIG
 // =============================================
-const API = "https://backendsite-production-6bcb.up.railway.app";
+const API = "gearlycomtr-backend-production.up.railway.app";
 
 // =============================================
 // STATE
